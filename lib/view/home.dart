b6efc4f0773705b8e02f89eda5e03d4422d53fa3
Aditi <adithakurti@gmail.com>
2023-06-22 14:57:08 +0530

    added the ui of the app

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/view/widget/NewsContainer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView.builder(
+        controller: PageController(
+          initialPage: 0,
+        ),
+        scrollDirection: Axis.vertical,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return NewsContainer(
+            imgUrl: "https://cdn.wionews.com/sites/default/files/styles/story_page/public/2023/06/22/361299-mofi-wer.png?imwidth=1920",
+            newsHead: "PM Modi in highlights: US planning to ease H1-B visa rule for Indians, say report",
+            newsDes: "PM Modi arrived in Washington DC on Wednesday on his second leg of the state visit to the US. He is expected to hold bilateral discussions with his American counterpart Joe Biden, and he will also address a joint session of the US Congress.",
+            newsUrl: "https://www.wionews.com/world/pm-modi-in-us-live-updates-indian-pm-interacts-with-indian-and-american-students-607104",
+          );
+        }),
+    );
   }
 }
---
+++ lib/view/widget/NewsContainer.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class NewsContainer extends StatelessWidget {
+
+  String imgUrl;
+  String newsHead;
+  String newsDes;
+  String newsUrl;
+
+    NewsContainer({super.key,
+    required this.imgUrl,
+    required this.newsHead,
+    required this.newsUrl,
+    required this.newsDes,
+   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+              Image.network(
+                height:400,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                imgUrl),
+
+                
+
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+
+              Text(newsHead,
+              style: TextStyle(fontSize: 22,
+              fontWeight: FontWeight.bold,
+              ),
+              ),
+              Text(newsDes,
+              style: TextStyle(fontSize: 16,),
+              ),
+
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ElevatedButton(
+                    onPressed: (){
+                      print("Going to $newsUrl");
+                    },
+                    child: Text("Read More"),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,)
+            ],),
+    );
+  }
+}
